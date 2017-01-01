53b4b6878bb5c12d51f0a331dbbe58826b167859
Bob van der Linden <bobvanderlinden@gmail.com>
2017-01-01 17:41:38 +0100

    avoid freeze when finding multiple networks with same ssid

--- src/main.ino
+++ src/main.ino
@@ -69,18 +69,79 @@ void handleMessageRequest(AsyncWebServerRequest *request, Message message) {
 }
 
 void wifiConnect() {
-  Serial.print("Connecting");
   WiFi.hostname(hostName);
-  WiFi.setAutoReconnect(true);
-  WiFi.begin(wifi_ssid, wifi_password);
+
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    digitalWrite(PIN_STATUS_LED, 1);
-    delay(90);
-    digitalWrite(PIN_STATUS_LED, 0);
-    delay(10);
+    // Scan networks
+    Serial.println("Scanning networks");
+    WiFi.scanDelete();
+    WiFi.scanNetworks(true, true);
+    while (WiFi.scanComplete() < 0) {
+      Serial.print(".");
+      digitalWrite(PIN_STATUS_LED, 1);
+      delay(50);
+      digitalWrite(PIN_STATUS_LED, 0);
+      delay(50);
+    }
+
+    // Finished scanning
+    int networkCount = WiFi.scanComplete();
+    Serial.println();
+    Serial.print("Found ");
+    Serial.print(networkCount);
+    Serial.println(" networks.");
+
+    // Go through each network and see whether we should connect to it.
+    for(int networkIndex=0; WiFi.status() != WL_CONNECTED && networkIndex<networkCount; networkIndex++) {
+      String ssid = WiFi.SSID(networkIndex);
+      int signal = WiFi.RSSI(networkIndex);
+      uint8* bssid = WiFi.BSSID(networkIndex);
+      int channel = WiFi.channel(networkIndex);
+
+      // Connect to the network when it matches wifi_ssid
+      if (ssid.equals(wifi_ssid)) {
+        Serial.print("Connecting to '");
+        Serial.print(ssid);
+        Serial.print("' (");
+        Serial.print(bssid[5], HEX);
+        Serial.print(":");
+        Serial.print(bssid[4], HEX);
+        Serial.print(":");
+        Serial.print(bssid[3], HEX);
+        Serial.print(":");
+        Serial.print(bssid[2], HEX);
+        Serial.print(":");
+        Serial.print(bssid[1], HEX);
+        Serial.print(":");
+        Serial.print(bssid[0], HEX);
+        Serial.print(") on channel ");
+        Serial.print(channel);
+        Serial.println();
+
+        // Specifically connect to this channel and bssid, so that
+        // we do not get ssid duplicates.
+        WiFi.begin(wifi_ssid, wifi_password, channel, bssid, true);
+        for(int timeout=0;WiFi.status() != WL_CONNECTED && timeout<50;timeout++) {
+          Serial.print(".");
+          digitalWrite(PIN_STATUS_LED, 0);
+          delay(50);
+          digitalWrite(PIN_STATUS_LED, 1);
+          delay(50);
+        }
+        Serial.println();
+
+        if (WiFi.status() != WL_CONNECTED) {
+          Serial.println("Failed to connect");
+        }
+      }
+    }
+
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Failed to connect to any of the scanned networks");
+      digitalWrite(PIN_STATUS_LED, 1);
+      delay(1000);
+    }
   }
-  Serial.println();
 
   Serial.println("Connected");
 
